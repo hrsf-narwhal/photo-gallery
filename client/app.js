6309c7ee346a1e87845b8c2e1c6c5d273f4792ca
mjcampagna <mjcampagna@gmail.com>
2018-06-03 20:54:14 -0700

    Makes a GET request to the server, and renders the response (images) to the page

--- client/app.js
+++ client/app.js
@@ -1,7 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+const React = require('react');
+const ReactDOM = require('react-dom');
 
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('app')
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      images: []
+    }
+  }
+
+  fetchImages() {
+    fetch('/images/1026', {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+    .then( response => response.json() )
+    .then( response => {
+      console.log(response)
+      this.setState({
+        images: response
+      });
+    })
+    .catch( err => console.log(err) );
+  } // fetchImages
+
+  componentDidMount() {
+    this.fetchImages();
+  }
+
+  render() {
+
+    const images = this.state.images.map( image => {
+      return (
+        <img src={image.image_url} />
+      );
+    });
+
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <p>{images}</p>
+      </div>
+    )
+  } // render
+
+} // App
+
+ReactDOM.render(<App />, document.getElementById('app'));
