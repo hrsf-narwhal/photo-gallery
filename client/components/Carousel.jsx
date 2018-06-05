75675bd6cc3fc7dcef27893e4a357e4361b50c78
mjcampagna <mjcampagna@gmail.com>
2018-06-05 02:27:51 -0700

    Improvements to the carousel

--- client/components/Carousel.jsx
+++ client/components/Carousel.jsx
@@ -7,9 +7,16 @@ import Arrow from './Arrow.jsx';
 import Slide from './Slide.jsx';
 
 const Container = styled.div`
-  display: inline-block;
+  background-color: #f5f5f5;
+  max-width: 570px;
   position: relative;
-  vertical-align: top;
+
+  &::after {
+    content: '';
+    display: block;
+    padding-top: 100%;
+    width: 100%;
+  }
 `;
 
 const Carousel = (props) => {
@@ -21,7 +28,7 @@ const Carousel = (props) => {
 
   return (
     <Container id="carousel">
-      <Slide images={props.images} current={props.current} />
+      <Slide images={props.images} current={props.current} next={props.next}/>
       <Arrow className="arrow-previous" action={(e) => props.previous(e)} glyph="P" />
       <Arrow className="arrow-next" action={(e) => props.next(e)} glyph="N" />
     </Container>
--- client/components/Slide.jsx
+++ client/components/Slide.jsx
@@ -4,19 +4,24 @@ const ReactDOM = require('react-dom');
 import styled from 'styled-components';
 
 const SlideContainer = styled.div`
+	align-items: center;
+	display: flex;
 	font-size: 0;
+	height: 100%;
+	position: absolute;
 `;
 
 const Img = styled.img`
 	display: block;
+	flex-grow: 0;
 `;
 
 const Slide = (props) => {
 	if ( props.images.length > 0 ) {
 		return (
-			<div id="slide">
+			<SlideContainer id="slide" onClick={(e) => props.next(e)}>
 				<Img src={props.images[ props.current ].image_url} />
-			</div>
+			</SlideContainer>
 		);
 	}
 	return null;
